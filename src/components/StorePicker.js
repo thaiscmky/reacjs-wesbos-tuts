a3de452bed745aa808b49d3f0ef5e97abf1d09ab
Thais <22459337+thaiscmky@users.noreply.github.com>
2018-05-12 09:09:39 -0500

    Setting up React4 Imparative Routing
    depends on prop-types

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,37 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class StorePicker extends React.Component {
+  constructor() {
+    super();
+    //bind StorePicker class to goToStore method
+    this.goToStore = this.goToStore.bind(this);
+  }
+  goToStore(e) {
+    e.preventDefault();
+    this.props.history.push(`/store/${this.storeInput.value}`);
+  }
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          ref={input => {
+            this.storeInput = input;
+          }}
+        />
         <button type="submit">Visit Store</button>
       </form>
     );
   }
 }
+
+//Surface router
+StorePicker.contextTypes = {
+  router: PropTypes.object
+};
 
 export default StorePicker;
